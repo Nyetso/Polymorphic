fd26c44b6a8f62318e815e784bc1454ca1f61c7c
Nyetso <harris_nazman@yaoo.com.sg>
2024-05-05 21:18:43 +0800

    tutorial

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,57 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Staff;
+use App\Models\Product;
+use App\Models\Photo;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/create', function(){
+    // $staff = new Staff;
+    // $staff->name = "Peter Diaz";
+    // $staff->save();
+
+    // $product = new Product;
+    // $product->name = "PHP Course";
+    // $product->save();
+
+    $staff = Staff::findOrFail(2);
+    $staff->photos()->create(['path'=>'exampletoassign.jpg']);
+
+});
+
+Route::get('/update', function(){
+    $staff = Staff::findOrFail(1);
+    foreach($staff->photos as $photo)
+    {
+        $photo->path = "example.update.jpg";
+        $photo->save();
+    }
+});
+
+Route::get('/read', function(){
+    $staff = Staff::findOrFail(1);
+    foreach($staff->photos as $photo)
+    {
+        return $photo;
+    }
+});
+
+Route::get('/delete', function(){
+    $staff = Staff::findOrFail(2);
+    foreach($staff->photos as $photo)
+    {
+        $photo->whereId(3)->delete();
+    }
+});
+
+Route::get('/assign', function(){
+    $staff = Staff::findOrFail(2);
+    $photo = Photo::findOrFail(4);
+
+    $staff->photos()->save($photo);
 });
